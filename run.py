c6bedd0a250a8ac8ee15d0ee43e6392985d749d6
CobaltConcrete <ganqingrong55@gmail.com>
2024-06-23 18:34:06 +0800

    update model in run.py

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import sys
 from YuNet import YuNet
 
 detector = YuNet(
-        model=r"models\postproc_yunet_top50_th60_360x640_sh4.blob",
+        model=r"models\face_detection_yunet_360x640_sh4.blob",
         model_resolution="360x640",
         input_src=r"IRON MAN 2 (2010) .mp4",
         # resolution=args.resolution,
